46b90e458f6846e3cc467fb3bc8f48e97fc25d95
ricardobizerra <ricardo.bizerra19@gmail.com>
2023-09-07 23:36:51 -0300

    feat(main): create prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+	public prismaClient: PrismaClient;
+
+	constructor() {
+		super({
+			log: ['warn', 'error']
+		});
+	}
+
+	onModuleInit() {
+		return this.$connect();
+	}
+
+	onModuleDestroy() {
+		throw this.$disconnect();
+	}
+}
