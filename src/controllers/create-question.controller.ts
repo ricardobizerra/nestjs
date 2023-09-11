c562d0a7b1476d376826144e8cc98a549cc72a28
ricardobizerra <ricardo.bizerra19@gmail.com>
2023-09-10 22:19:12 -0300

    feat(main): create question controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UserPayload } from 'src/auth/jwt.strategy';
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { z } from 'zod';
+
+const createQuestionBodySchema = z.object({
+	title: z.string(),
+	content: z.string()
+});
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
@@ -11,10 +22,47 @@ export class QuestionController {
         private prisma: PrismaService
 	) {}
 
+	private convertToSlug(title: string): string {
+		const normalizedString = title
+			.normalize('NFD')
+			.replace(/[\u0300-\u036f]/g, '');
+      
+		const slug = normalizedString
+			.toLowerCase()
+			.replace(/[^a-z0-9]+/g, '-')
+			.replace(/(^-|-$)+/g, '');
+      
+		return slug;
+	}
+
     @Post()
 	async createQuestion(
+        @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
         @CurrentUser() user: UserPayload 
 	) {
-		return user;
+		const {
+			title,
+			content
+		} = body;
+
+		const userId = user.sub;
+
+		const slugTitle = this.convertToSlug(title);
+		const slugDate = Date.now();
+
+		const slug = `${slugDate}-${userId}-${slugTitle}`;
+
+		await this.prisma.question.create({
+			data: {
+				authorId: userId,
+				title,
+				content,
+				slug,
+			}
+		});
+
+		return {
+			question_slug: slug
+		};
 	}
 }
